c6e8aa16ca56dc8f625fafafef5902fb8adaa6bb
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-09 15:17:32 +0000

    Fixed an error

--- hello_world/views.py
+++ hello_world/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 def index(request):
     if request.method == 'POST':
         return HttpResponse("You must have POSTed something.")
-        else:
-            return HttpResponse("request.method")
+    else:
+        return HttpResponse("request.method")
